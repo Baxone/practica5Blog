91e7a920121054f7506b9ac4e4b7576bc06fcf59
Juan Antonio Pérez Jarillo <jantonio.perez@estudioclandestino.es>
2023-07-24 20:26:48 +0200

    Lista de post separado en un componente funcional

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,22 +1,8 @@
 import { Component } from '@angular/core';
-import { Post } from 'src/app/interfaces/post.interface';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
-export class BlogComponent {
-  //formulario
-  newpost: Post = { titulo: "", imagen: "", fecha: "", texto: "" }
-
-  getDataForm() {
-    if (this.newpost.titulo !== "" && this.newpost.imagen !== "" && this.newpost.fecha !== "" && this.newpost.texto !== "") {
-      this.arrPost.push(this.newpost);
-      this.newpost = { titulo: "", imagen: "", fecha: "", texto: "" }
-    } else {
-      alert('Los campos son obligatorios')
-    }
-  }
-
-}
+export class BlogComponent { }
